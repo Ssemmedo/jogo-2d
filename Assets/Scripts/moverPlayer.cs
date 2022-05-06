09fbe7aac0c5d5c8d0ccd55e519b39dc769712ba
sandro <sandro.semedo@alunos.unis.edu.br>
2022-05-06 19:24:08 -0300

    acompanhando com o projeto da mirella

---
+++ Assets/Scripts/moverPlayer.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moverPlayer : MonoBehaviour
+{
+
+    public float velocidade;
+    
+    public Animator anim;
+
+    public static int pontuacao;
+
+    public Text pontuacaoTela;
+
+    public Text pontuacaoTelaSombra;
+
+    private BoxCollider2D caixaColisao;
+
+    private GameObject obstaculo;
+
+    public Rigidbody2D rdb2D;
+
+    public static int moedas;
+
+    public AudioClip efeitoSonoroMoedas;
+    public AudioClip efeitoSonoroGato;
+    public AudioClip efeitoSonoroItem;
+    private AudioSource audioSource;
+
+    public Text quantidadeMoedas;
+    public Text quantidadeMoedasSombra;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        caixaColisao = GetComponent<BoxCollider2D>();
+
+        pontuacao = 0;
+
+        moedas = 0;
+
+        obstaculo = GameObject.FindGameObjectWithTag("Obstaculos");
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            rdb2D.AddForce(transform.up * 400);
+
+            anim.SetTrigger("pulando");
+
+        }
+        else 
+        {
+            anim.SetTrigger("correndo");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col) 
+    {
+        if (col.tag == "Obstaculos") 
+        {
+            Application.LoadLevel("gameover");
+        }
+    }
+
+    public void adicionarMoedas() 
+    {
+        moedas++;
+
+        quantidadeMoedas.text = moedas.ToString();
+        quantidadeMoedasSombra.text = moedas.ToString();
+
+        audioSource.PlayOneShot(efeitoSonoroMoedas);
+
+    }
+
+    public void adicionarPontos() 
+    {
+        pontuacao++;
+
+        audioSource.PlayOneShot(efeitoSonoroItem);
+    }
+
+
+}
